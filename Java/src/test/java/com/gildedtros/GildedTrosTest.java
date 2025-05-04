285caeda289ce492002d42d875a315fff5a0af04
Duncan Casteleyn <10881109+DuncanCasteleyn@users.noreply.github.com>
2025-05-04 14:27:08 +0200

    test: Improve the negative quality test & Add a test to never increase above fifty

--- Java/src/test/java/com/gildedtros/GildedTrosTest.java
+++ Java/src/test/java/com/gildedtros/GildedTrosTest.java
@@ -4,39 +4,42 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GildedTrosTest {
 
     private static final int LEGENDARY_ITEM_QUALITY = 80;
-
-    Item[] AllItems = new Item[]{
-            new Item("Ring of Cleansening Code", 10, 20),
-            new Item("Good Wine", 2, 0),
-            new Item("Elixir of the SOLID", 5, 7),
-            new Item("B-DAWG Keychain", 0, 80),
-            new Item("B-DAWG Keychain", -1, 80),
-            new Item("Backstage passes for Re:Factor", 15, 20),
-            new Item("Backstage passes for Re:Factor", 10, 49),
-            new Item("Backstage passes for HAXX", 5, 49),
-            // these smelly items do not work properly yet
-            new Item("Duplicate Code", 3, 6),
-            new Item("Long Methods", 3, 6),
-            new Item("Ugly Variable Names", 3, 6)
-    };
+    private static final int MAX_QUALITY = 50;
 
     @Test
     void updateQuality_WhenExecuted_QualityShouldNeverBeNegative() {
         // Given
-        GildedTros app = new GildedTros(AllItems);
+        Item[] items = new Item[]{
+                new Item("Ring of Cleansening Code", 10, 0),
+                new Item("Good Wine", 2, 0),
+                new Item("Elixir of the SOLID", 5, 0),
+                new Item("B-DAWG Keychain", 0, 80),
+                new Item("B-DAWG Keychain", -1, 80),
+                new Item("Backstage passes for Re:Factor", 15, 0),
+                new Item("Backstage passes for Re:Factor", 10, 0),
+                new Item("Backstage passes for HAXX", 5, 0),
+                // these smelly items do not work properly yet
+                new Item("Duplicate Code", 3, 0),
+                new Item("Long Methods", 3, 0),
+                new Item("Ugly Variable Names", 3, 0)
+        };
+
+        GildedTros app = new GildedTros(items);
 
         // When
         app.updateQuality();
 
         // Then
         final int lowestQuality = Arrays.stream(app.items)
-                                         .mapToInt(item -> item.quality)
-                                         .min().orElseThrow(() -> new RuntimeException("No quality found"));
+                                        .mapToInt(item -> item.quality)
+                                        .min()
+                                        .orElseThrow(() -> new IllegalStateException("No quality found"));
 
         assertTrue(lowestQuality >= 0);
     }
@@ -59,6 +62,32 @@ class GildedTrosTest {
         assertEquals(LEGENDARY_ITEM_QUALITY, app.items[1].quality);
         assertEquals(0, app.items[0].sellIn);
         assertEquals(-1, app.items[1].sellIn);
+    }
+
+    @Test
+    void updateQuality_WhenExecuted_QualityShouldNeverIncreaseAboveFifty() {
+        // Given
+        Item[] AllItems = new Item[]{
+                new Item("Good Wine", 2, MAX_QUALITY),
+                new Item("Good Wine", 2, 49),
+                new Item("Backstage passes for Re:Factor", 10, 49),
+                new Item("Backstage passes for Re:Factor", 10, 48),
+                new Item("Backstage passes for HAXX", 5, 49),
+                new Item("Backstage passes for HAXX", 5, 47)
+        };
+
+        GildedTros app = new GildedTros(AllItems);
+
+        // When
+        app.updateQuality();
+
+        // Then
+        assertEquals(MAX_QUALITY, app.items[0].quality);
+        assertEquals(MAX_QUALITY, app.items[1].quality);
+        assertEquals(MAX_QUALITY, app.items[2].quality);
+        assertEquals(MAX_QUALITY, app.items[3].quality);
+        assertEquals(MAX_QUALITY, app.items[4].quality);
+        assertEquals(MAX_QUALITY, app.items[5].quality);
     }
 
 }
