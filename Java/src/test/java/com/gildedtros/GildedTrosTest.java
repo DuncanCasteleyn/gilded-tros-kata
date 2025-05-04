7f7bd2e1b597aaa21dbd117a41931e5574b00897
Duncan Casteleyn <10881109+DuncanCasteleyn@users.noreply.github.com>
2025-05-04 14:11:04 +0200

    test: Add test to check that nothing changes to the legendary item

--- Java/src/test/java/com/gildedtros/GildedTrosTest.java
+++ Java/src/test/java/com/gildedtros/GildedTrosTest.java
@@ -8,6 +8,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GildedTrosTest {
 
+    private static final int LEGENDARY_ITEM_QUALITY = 80;
+
     Item[] AllItems = new Item[]{
             new Item("Ring of Cleansening Code", 10, 20),
             new Item("Good Wine", 2, 0),
@@ -37,6 +39,26 @@ class GildedTrosTest {
                                          .min().orElseThrow(() -> new RuntimeException("No quality found"));
 
         assertTrue(lowestQuality >= 0);
+    }
+
+    @Test
+    void updateQuality_WhenExecuted_NothingShouldChangeForLegendaryItem() {
+        // Given
+        Item[] AllItems = new Item[]{
+                new Item("B-DAWG Keychain", 0, 80),
+                new Item("B-DAWG Keychain", -1, 80)
+        };
+
+        GildedTros app = new GildedTros(AllItems);
+
+        // When
+        app.updateQuality();
+
+        // Then
+        assertEquals(LEGENDARY_ITEM_QUALITY, app.items[0].quality);
+        assertEquals(LEGENDARY_ITEM_QUALITY, app.items[1].quality);
+        assertEquals(0, app.items[0].sellIn);
+        assertEquals(-1, app.items[1].sellIn);
     }
 
 }
